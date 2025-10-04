f7fb3c748396371075d8bc2d5b93938e40df0d86
Marco Kuidja <kuidjamarco@gmail.com>
2025-10-04 07:45:00 +0100

    Refactor WinRM validation command: remove unused port and cert thumbprint parameters for cleaner execution in validate-host module.

--- terraform/modules/validate-host/main.tf
+++ terraform/modules/validate-host/main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "validate_winrm" {
   provisioner "local-exec" {
-    command = "${path.root}/../scripts/check-winrm.sh -TargetHost ${var.host} -Port ${var.port} -User ${var.user} -Password '${var.password}' ${var.https ? "-Https" : ""} ${var.use_ntlm ? "-UseNtlm" : ""} ${var.production ? "-Production" : ""} -CertThumbprint '${var.cert_thumbprint}'"
+    command = "${path.root}/../scripts/check-winrm.sh -TargetHost ${var.host} -User ${var.user} -Password '${var.password}' ${var.https ? "-Https" : ""} ${var.use_ntlm ? "-UseNtlm" : ""} ${var.production ? "-Production" : ""}"
   }
 }
