3f99a3084affa4f570a4743789180c524af214e4
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2025-10-18 19:28:54 +0200

    chore: types

--- terraform/modules/hyperv-host/main.tf
+++ terraform/modules/hyperv-host/main.tf
@@ -6,13 +6,13 @@ module "vms" {
     hyperv = hyperv
   }
 
-  name            = try(each.value.name, var.default_memory)
-  mac             = try(each.value.mac, var.vms_macs[each.value.ip])
+  name            = try(coalesce(each.value.name, var.default_memory))
+  mac             = try(coalesce(each.value.mac, var.vms_macs[each.value.ip]))
   memory          = try(coalesce(each.value.memory, var.default_memory))
   cpus            = each.value.cpus
   disk_gb         = each.value.disk_gb
   storage_disk_gb = each.value.storage_disk_gb
   iso_path        = var.iso_paths[each.value.ip]
   cluster_switch  = var.cluster_switch
-  disk_dir_path   = try(each.value.disk_dir_path, var.default_disk_dir_path)
+  disk_dir_path   = try(coalesce(each.value.disk_dir_path, var.default_disk_dir_path))
 }
