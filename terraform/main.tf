b71faca7eecc50de1e0d49c54c3fa1177b06a4e2
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2025-10-18 19:15:01 +0200

    chore: types

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@ module "talos_image_factory" {
   machines_configs = {
     for vm in var.host_vms : vm.ip => {
       host_name   = vm.name
-      mac_address = try(coalesce(vm.mac, null), local.vms_macs[vm.ip])
-      extensions  = try(coalesce(vm.extensions, null), var.default_talos_extensions)
+      mac_address = coalesce(vm.mac, local.vms_macs[vm.ip])
+      extensions  = coalesce(vm.extensions, var.default_talos_extensions)
     }
   }
 
