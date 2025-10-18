3afcba585f5ed84995f436c1464db7750f322c04
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2025-10-18 19:20:31 +0200

    chore: types

--- terraform/modules/talos-image-factory/outputs.tf
+++ terraform/modules/talos-image-factory/outputs.tf
@@ -1,20 +1,20 @@
 output "iso_urls" {
   value = {
-    for ip in var.machines_configs : ip => data.talos_image_factory_urls.this[ip].urls.iso_secureboot
+    for ip, v in var.machines_configs : ip => data.talos_image_factory_urls.this[ip].urls.iso_secureboot
   }
   description = "The URL of the generated Talos secure boot ISO image."
 }
 
 output "schematic_ids" {
   value = {
-    for ip in var.machines_configs : ip => talos_image_factory_schematic.this[ip].id
+    for ip, v in var.machines_configs : ip => talos_image_factory_schematic.this[ip].id
   }
   description = "The ID of the generated Talos schematic."
 }
 
 output "talos_installers" {
   value = {
-    for ip in var.machines_configs : ip => data.talos_image_factory_urls.this[ip].urls.installer_secureboot
+    for ip, v in var.machines_configs : ip => data.talos_image_factory_urls.this[ip].urls.installer_secureboot
   }
   description = "The Talos installer image URL."
 }
