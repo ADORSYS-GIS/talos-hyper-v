b7d2cead77515e11a779c97bc5b7c107dac8f9b0
Marco Kuidja <kuidjamarco@gmail.com>
2026-01-12 11:48:21 +0100

    feat(hyperv): add support for 2nd host

--- terraform/hyperv_hosts.tf
+++ terraform/hyperv_hosts.tf
@@ -13,6 +13,21 @@ module "hyperv-host01" {
   }
 }
 
+module "hyperv-host02" {
+  source = "./modules/hyperv-host"
+
+  host_config           = var.hyperv_hosts["host02"]
+  cluster_switch        = var.switch
+  vms_macs              = local.vms_macs
+  default_cpus          = var.default_cpus
+  default_memory        = var.default_memory
+  default_disk_dir_path = var.disk_dir_path
+  vms                   = { for vm in local.vms_by_host["host02"] : vm.name => vm }
+  iso_paths = {
+    for k, s in local.iso_name_suffix : k => "${var.iso_prefix_path}\\${s}"
+  }
+}
+
 locals {
-  host_modules = [module.hyperv-host01]
+  host_modules = [module.hyperv-host01, module.hyperv-host02]
 }
